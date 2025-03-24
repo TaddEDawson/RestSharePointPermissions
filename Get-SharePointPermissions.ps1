228c6959be99b213757f37f3ca68272e3457521f
Tadd E. Dawson <tadd@microsoft.com>
2025-03-24 16:46:01 -0400

    premeeting

--- Get-SharePointPermissions.ps1
+++ Get-SharePointPermissions.ps1
@@ -1,80 +1,151 @@
 <#
-.SYNOPSIS
-Retrieves SharePoint permissions and logs results.
+    .SYNOPSIS
+    Retrieves SharePoint permissions and logs results.
 
-.DESCRIPTION
-Use this function to gather information about site collection administrators, webs, lists, and items with unique permissions.
+    .DESCRIPTION
+    Use this function to gather information about site collection administrators, webs, lists, and items with unique permissions.
 
-.PARAMETER SiteCollectionUrl
-The URL of the SharePoint site collection.
+    .PARAMETER SiteCollectionUrl
+    The URL of the SharePoint site collection.
 
-.EXAMPLE
-Get-SharePointPermissions -SiteCollectionUrl "https://yoursharepointsite"
+    .EXAMPLE
+    Get-SharePointPermissions -SiteCollectionUrl "https://yoursharepointsite"
 
-.NOTES
-Author: (Your Name)
+    .NOTES
+    Author: (Your Name)
 #>
-function Get-SharePointPermissions {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$SiteCollectionUrl
-    )
-
-    $headers = @{
-        "Accept" = "application/json;odata=verbose"
+[CmdletBinding()]
+[OutputType([PSCustomObject])]
+param
+( 
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$SiteCollectionUrl
+) # param block
+begin
+{
+    # Define a custom object to hold SharePoint permissions
+    class SharePointPermission {
+        [string]$Url
+        [string]$Type
+        [string]$UserName
+        [string]$PermissionLevel
+    
+        SharePointPermission([string]$url, [string]$type, [string]$userName, [string]$permissionLevel) {
+            $this.Url = $url
+            $this.Type = $type
+            $this.UserName = $userName
+            $this.PermissionLevel = $permissionLevel
+        }
     }
 
-    # Get Site Collection Administrators
-    try {
-        $siteAdmins = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/siteusers" -Headers $headers -UseDefaultCredentials
-        $siteAdmins = $siteAdmins.d.results | Where-Object { $_.IsSiteAdmin -eq $true }
+    # Function to log messages
+    function Write-Log {
+        param(
+            [Parameter(Mandatory = $true)]
+            [string]$Message,
+            [Parameter(Mandatory = $false)]
+            [ValidateNotNullOrEmpty()]
+            [string]$LogFile = "$env:TEMP\SharePointPermissions.log"
+        )
+        $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+        $logMessage = "$timestamp - $Message"
 
-        foreach ($admin in $siteAdmins) {
-            [SharePointPermission]::new($SiteCollectionUrl, "Site Collection Administrator", $admin.LoginName, "Full Control")
-            Write-Log -Message "Found Site Collection Administrator: $($admin.LoginName)"
-        }
-    } catch {
-        Write-Log -Message "Error retrieving site collection administrators: $($_.Exception.Message)"
-    }
-
-    # Get all webs with unique permissions
-    try {
-        $webs = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/webs" -Headers $headers -UseDefaultCredentials
-        $webs = $webs.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
-
-        foreach ($web in $webs) {
-            [SharePointPermission]::new($web.Url, "Web with Unique Permissions", "", "")
-            Write-Log -Message "Found Web with Unique Permissions: $($web.Url)"
-        }
-    } catch {
-        Write-Log -Message "Error retrieving webs: $($_.Exception.Message)"
-    }
-
-    # Get lists with unique permissions
-    try {
-        $lists = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/lists" -Headers $headers -UseDefaultCredentials
-        $lists = $lists.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
-
-        foreach ($list in $lists) {
-            [SharePointPermission]::new($list.DefaultViewUrl, "List with Unique Permissions", "", "")
-            Write-Log -Message "Found List with Unique Permissions: $($list.DefaultViewUrl)"
-        }
-    } catch {
-        Write-Log -Message "Error retrieving lists: $($_.Exception.Message)"
-    }
-
-    # Get list items with unique permissions
-    foreach ($list in $lists) {
-        try {
-            $items = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/lists(guid'$($list.Id)')/items" -Headers $headers -UseDefaultCredentials
-            $items = $items.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
-
-            foreach ($item in $items) {
-                [SharePointPermission]::new($item.FileRef, "List Item with Unique Permissions", "", "")
-                Write-Log -Message "Found List Item with Unique Permissions: $($item.FileRef)"
-            }
-        } catch {
-            Write-Log -Message "Error retrieving items for list $($list.Title): $($_.Exception.Message)"
-        }
+        Write-Host $logMessage
+        Add-Content -Path $LogFile -Value $logMessage
     }
 }
+} # begin block
+process
+{
+    function Get-SharePointPermissions 
+    {
+        [CmdletBinding()]
+        param 
+        (
+            [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+            [string]$SiteCollectionUrl
+        ) # param block
+    
+        process 
+        {
+            $headers = @{ "Accept" = "application/json;odata=verbose" }
+    
+            # Get Site Collection Administrators
+            try 
+            {
+                $siteAdmins = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/siteusers" -Headers $headers -UseDefaultCredentials
+                $siteAdmins = $siteAdmins.d.results | Where-Object { $_.IsSiteAdmin -eq $true }
+    
+                foreach ($admin in $siteAdmins) 
+                {
+                    [SharePointPermission]::new($SiteCollectionUrl, "Site Collection Administrator", $admin.LoginName, "Full Control")
+                    Write-Log -Message "Found Site Collection Administrator: $($admin.LoginName)"
+                } # foreach ($admin in $siteAdmins)
+            } # try block
+            catch 
+            {
+                Write-Log -Message "Error retrieving site collection administrators: $($_.Exception.Message)"
+            } # catch block
+    
+            # Get all webs with unique permissions
+            try 
+            {
+                $webs = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/webs" -Headers $headers -UseDefaultCredentials
+                $webs = $webs.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
+    
+                foreach ($web in $webs) 
+                {
+                    [SharePointPermission]::new($web.Url, "Web with Unique Permissions", "", "")
+                    Write-Log -Message "Found Web with Unique Permissions: $($web.Url)"
+                } # foreach ($web in $webs)
+            } # try block
+            catch 
+            {
+                Write-Log -Message "Error retrieving webs: $($_.Exception.Message)"
+            } # catch block
+    
+            # Get lists with unique permissions
+            try 
+            {
+                $lists = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/lists" -Headers $headers -UseDefaultCredentials
+                $lists = $lists.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
+    
+                foreach ($list in $lists) 
+                {
+                    [SharePointPermission]::new($list.DefaultViewUrl, "List with Unique Permissions", "", "")
+                    Write-Log -Message "Found List with Unique Permissions: $($list.DefaultViewUrl)"
+                } # foreach ($list in $lists)
+            } 
+            catch 
+            {
+                Write-Log -Message "Error retrieving lists: $($_.Exception.Message)"
+            } # catch block
+    
+            # Get list items with unique permissions
+            foreach ($list in $lists) 
+            {
+                try 
+                {
+                    $items = Invoke-RestMethod -Uri "$SiteCollectionUrl/_api/web/lists(guid'$($list.Id)')/items" -Headers $headers -UseDefaultCredentials
+                    $items = $items.d.results | Where-Object { $_.HasUniqueRoleAssignments -eq $true }
+    
+                    foreach ($item in $items) 
+                    {
+                        [SharePointPermission]::new($item.FileRef, "List Item with Unique Permissions", "", "")
+                        Write-Log -Message "Found List Item with Unique Permissions: $($item.FileRef)"
+                    } # foreach ($item in $items)
+                } # try block
+                catch 
+                {
+                    Write-Log -Message "Error retrieving items for list $($list.Title): $($_.Exception.Message)"
+                } # catch block
+            } # foreach ($list in $lists)
+        } # process block
+    } # function Get-SharePointPermissions 
+    
+} # process block
+end
+{
+
+} # end block
+
